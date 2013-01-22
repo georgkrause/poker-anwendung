ca2f7de6ae2c22f6d8868ebe0fbcf2a4bcae1990
Georg Krause <mail@georg-krause.net>
2013-01-22 23:27:05 +0100

    deleted useless code

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,21 +1,8 @@
 package core;
 
-import gui.Fenstor;
-
 public class Main {
 
 	public static void main(String Args[]) {
-		Game game = new Game();
-		new Fenstor(game);
-		for (int i = 0; i < 10; i++) {
-			Card card = game.deal();
-			System.out.println(card.getName());
-		}
-		game.tableCards[0].discover();
-		game.tableCards[0].getPicture();
-		
-		game.activePlayers[0].getCards()[0].getPicture();
-		
+		new Game();
 	}
-
 }
