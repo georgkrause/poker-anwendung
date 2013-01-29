fd59b8fdb3146acb867f7f228a0c5a3a1efd9015
Georg Krause <mail@georg-krause.net>
2013-01-29 21:34:13 +0100

    changes from the lesson

---
+++ src/core/Alfi.java
@@ -0,0 +1,16 @@
+package core;
+
+import java.util.Random;
+
+public class Alfi extends Player {
+
+	Alfi(int credit) {
+		super(credit);
+	}
+	
+	public int decide() {
+		Random r = new Random();
+		return r.nextInt(4);
+	}
+
+}
--- src/core/Game.java
+++ src/core/Game.java
@@ -9,97 +9,57 @@ import core.Player;
 
 public class Game {
 
-	private int credit = 1000;
 	public int cue = this.minimumBet;
+	public Card[] tableCards = new Card[5];
+
+	private int credit = 1000;
+	private final int minimumBet = 100;
 
 	private Card[] cards = new Card[52];
 	private int givenCards = 0;
 
-	private final int minimumBet = 100;
 	private int smallBlind;
 	private int bigBlind;
 	private int dealer;
 	private int turnPlayer;
 
 	private int pot;
-	public Card[] tableCards = new Card[5];
-
 	public Player[] activePlayers = new Player[4];
 	private int activePlayerNumber = 4;
-	
-	Window window;
 
-	Random r = new Random();
+	private Window window;
+
+	private Random r = new Random();
 
 	/**
 	 * initialize a new game - creates all the cards - choose five table cards
 	 */
 	Game() {
 
-		for (int colorCounter = 0; colorCounter < (Card.allColors.length); colorCounter++) {
-			for (int worthCounter = 0; worthCounter < (Card.allWorths.length); worthCounter++) {
-				int id = (worthCounter * Card.allColors.length + colorCounter + 1) - 1;
-				this.cards[id] = new Card(colorCounter, worthCounter);
-			}
-		}
+		// Generate all cards
+		this.generateCards();
 
-		for (int i = 0; i < 4; i++) {
-			this.activePlayers[i] = new Player(this.credit);
-		}
+		// create objects of the players
+		this.generatePlayers();
 
-		dealer = r.nextInt(4);
+		this.round();
+	}
 
-		if ((dealer + 1) > 3)
-			smallBlind = dealer + 1 - 4;
-		else
-			smallBlind = dealer + 1;
+	private void round() {
+		this.assignRoles();
 
-		if ((dealer + 2) > 3)
-			bigBlind = dealer + 2 - 4;
-		else
-			bigBlind = dealer + 2;
+		this.prepairPlayers();
 
-		turnPlayer = dealer;
-
-		// Karten geben und Spieler wieder ins Spiel bringen
-		for (int i = 0; i < 4; i++) {
-			if (activePlayers[i] != null) {
-				Card[] cards = { deal(), deal() };
-				activePlayers[i].setCards(cards);
-				activePlayers[i].folded = false;
-			}
-		}
-
-		this.activePlayers[0].getCards()[0].discover();
-		this.activePlayers[0].getCards()[1].discover();
-
+		// choose table cards
 		this.tableCards[0] = this.deal();
 		this.tableCards[1] = this.deal();
 		this.tableCards[2] = this.deal();
 		this.tableCards[3] = this.deal();
 		this.tableCards[4] = this.deal();
 
-//<<<<<<< HEAD
-		Window window = new Window(this);
-		int[] feld=sortworth(tableCards[0].getWorthID(),tableCards[1].getWorthID(),tableCards[2].getWorthID(),tableCards[3].getWorthID(),tableCards[4].getWorthID(),
-				activePlayers[0].getCards()[0].getWorthID(),activePlayers[0].getCards()[1].getWorthID());
-		System.out.println(feld[0]); System.out.println(feld[1]); System.out.println(feld[2]); System.out.println(feld[3]); System.out.println(feld[4]); System.out.println(feld[5]); System.out.println(feld[6]); 
-//		System.out.println(fivecolor(tableCards[0].getColorID(),tableCards[1].getColorID(),tableCards[2].getColorID(),tableCards[3].getColorID(),tableCards[4].getColorID(),
-//				activePlayers[0].getCards()[0].getColorID(),activePlayers[0].getCards()[1].getColorID()));
-		int [] feld2=sameworth(feld);
-		System.out.println(feld2[0]); System.out.println(feld2[1]); System.out.println(feld2[2]); System.out.println(feld2[3]);  
-
-		
-		
-//=======
 		window = new Window(this);
-		sortworth(tableCards[0].getWorthID(),tableCards[1].getWorthID(),tableCards[2].getWorthID(),tableCards[3].getWorthID(),tableCards[4].getWorthID(),
-				activePlayers[0].getCards()[0].getWorthID(),activePlayers[0].getCards()[1].getWorthID());
-		System.out.println(fivecolor(tableCards[0].getColorID(),tableCards[1].getColorID(),tableCards[2].getColorID(),tableCards[3].getColorID(),tableCards[4].getColorID(),
-				activePlayers[0].getCards()[0].getColorID(),activePlayers[0].getCards()[1].getColorID()));
 
 		this.collectBlinds();
-//>>>>>>> dev-basti
 
 		while (activePlayerNumber > 1 && !this.tableCards[4].isVisible()) {
 			int movesWithoutRaise = 0;
@@ -112,9 +72,12 @@ public class Game {
 
 				if (!activePlayers[turnPlayer].folded) {
 					if (turnPlayer != 0) {
-						choice = 1;
+						choice = ((Alfi) this.activePlayers[turnPlayer])
+								.decide();
 					} else {
-						choice = window.DialogBox();
+						do {
+							choice = window.DialogBox();
+						} while (choice < 0);
 					}
 
 					switch (choice) {
@@ -149,27 +112,83 @@ public class Game {
 				else
 					turnPlayer = turnPlayer + 1;
 			}
-			
-			if(!this.tableCards[2].isVisible()) {
-				for(int i = 0; i < 3; i++) {
+
+			if (!this.tableCards[2].isVisible()) {
+				for (int i = 0; i < 3; i++) {
 					this.tableCards[i].discover();
 					window.updateCommunityCards();
-				} 
+				}
 			} else if (!this.tableCards[3].isVisible()) {
 				this.tableCards[3].discover();
 				window.updateCommunityCards();
-			} else if(!this.tableCards[4].isVisible()) {
+			} else if (!this.tableCards[4].isVisible()) {
 				this.tableCards[4].discover();
 				window.updateCommunityCards();
 			}
 		}
+
+	}
+
+	private void prepairPlayers() {
+		// Karten geben und Spieler wieder ins Spiel bringen
+		for (int i = 0; i < 4; i++) {
+			if (activePlayers[i] != null) {
+				Card[] cards = { deal(), deal() };
+				activePlayers[i].setCards(cards);
+				activePlayers[i].folded = false;
+			}
+		}
+		// show cards of the human player
+		this.activePlayers[0].getCards()[0].discover();
+		this.activePlayers[0].getCards()[1].discover();
+
+	}
+
+	private void assignRoles() {
+		// choose dealer
+		dealer = r.nextInt(4);
+
+		// choose Blinds
+		if ((dealer + 1) > 3)
+			smallBlind = dealer + 1 - 4;
+		else
+			smallBlind = dealer + 1;
+
+		if ((dealer + 2) > 3)
+			bigBlind = dealer + 2 - 4;
+		else
+			bigBlind = dealer + 2;
+
+		// choose first Player
+		turnPlayer = dealer;
+
+	}
+
+	private void generatePlayers() {
+		for (int i = 0; i < 4; i++) {
+			if (i == 0) {
+				this.activePlayers[i] = new Player(this.credit);
+			} else {
+				this.activePlayers[i] = new Alfi(this.credit);
+			}
+		}
+
 	}
 
 	private void collectBlinds() {
 		this.activePlayers[this.bigBlind].raise(minimumBet);
-		this.activePlayers[this.smallBlind].raise(minimumBet/2);
+		this.activePlayers[this.smallBlind].raise(minimumBet / 2);
 		window.updateCredits();
-		this.raisePot((int) (minimumBet*1.5));
+		this.raisePot((int) (minimumBet * 1.5));
+	}
+
+	private void generateCards() {
+		for (int colorCounter = 0; colorCounter < (Card.allColors.length); colorCounter++) {
+			for (int worthCounter = 0; worthCounter < (Card.allWorths.length); worthCounter++) {
+				int id = (worthCounter * Card.allColors.length + colorCounter + 1) - 1;
+				this.cards[id] = new Card(colorCounter, worthCounter);
+			}
+		}
 	}
 
 	/**
@@ -268,16 +287,29 @@ public class Game {
 	public int getActivePlayerNumber() {
 		return activePlayerNumber;
 	}
-	
-	public int[] sortworth(int ccard0,int ccard1,int ccard2,int ccard3,int ccard4,int pcard0,int pcard1){
+
+	/**
+	 * sorts the cards
+	 * 
+	 * @param ccard0
+	 * @param ccard1
+	 * @param ccard2
+	 * @param ccard3
+	 * @param ccard4
+	 * @param pcard0
+	 * @param pcard1
+	 * @return
+	 */
+	public int[] sortworth(int ccard0, int ccard1, int ccard2, int ccard3,
+			int ccard4, int pcard0, int pcard1) {
 		int[] allcardsworth = new int[7];
-		allcardsworth[0]=ccard0;
-		allcardsworth[1]=ccard1;
-		allcardsworth[2]=ccard2;
-		allcardsworth[3]=ccard3;
-		allcardsworth[4]=ccard4;
-		allcardsworth[5]=pcard0;
-		allcardsworth[6]=pcard1;
+		allcardsworth[0] = ccard0;
+		allcardsworth[1] = ccard1;
+		allcardsworth[2] = ccard2;
+		allcardsworth[3] = ccard3;
+		allcardsworth[4] = ccard4;
+		allcardsworth[5] = pcard0;
+		allcardsworth[6] = pcard1;
 		for (int a = 0; a < 7; a++) {
 			int z = a;
 			while ((z > 0) && (allcardsworth[z] < allcardsworth[z - 1])) {
@@ -287,102 +319,134 @@ public class Game {
 				z = z - 1;
 			}
 		}
-		
+
 		return allcardsworth;
-		
-		}
-	
-	public int fivecolor(int ccard0,int ccard1,int ccard2,int ccard3,int ccard4,int pcard0,int pcard1){
+
+	}
+
+	/**
+	 * returns wheather there are five cards from the same color
+	 * 
+	 * @param ccard0
+	 * @param ccard1
+	 * @param ccard2
+	 * @param ccard3
+	 * @param ccard4
+	 * @param pcard0
+	 * @param pcard1
+	 * @return
+	 */
+	public int fivecolor(int ccard0, int ccard1, int ccard2, int ccard3,
+			int ccard4, int pcard0, int pcard1) {
 		int[] allcardscolor = new int[7];
-		allcardscolor[0]=ccard0;
-		allcardscolor[1]=ccard1;
-		allcardscolor[2]=ccard2;
-		allcardscolor[3]=ccard3;
-		allcardscolor[4]=ccard4;
-		allcardscolor[5]=pcard0;
-		allcardscolor[6]=pcard1;
-		int herz=0;
-		int kreuz=0;
-		int karo=0;
-		int pik=0;
-		
+		allcardscolor[0] = ccard0;
+		allcardscolor[1] = ccard1;
+		allcardscolor[2] = ccard2;
+		allcardscolor[3] = ccard3;
+		allcardscolor[4] = ccard4;
+		allcardscolor[5] = pcard0;
+		allcardscolor[6] = pcard1;
+		int herz = 0;
+		int kreuz = 0;
+		int karo = 0;
+		int pik = 0;
+
 		for (int a = 0; a < 7; a++) {
-			
+
 			switch (allcardscolor[a]) {
-			case 0: 
-				herz=herz+1;
+			case 0:
+				herz = herz + 1;
 				break;
-			
-			case 1: 
-				kreuz=kreuz+1;
+
+			case 1:
+				kreuz = kreuz + 1;
 				break;
-				
-			case 2: 
-				karo=karo+1;
+
+			case 2:
+				karo = karo + 1;
 				break;
-				
-			case 3: 
-				pik=pik+1;
+
+			case 3:
+				pik = pik + 1;
 				break;
-				
-			}}
-		
-		if(herz>=5){
-			return 0; 
-		}
-		
-		if(kreuz>=5){
-			return 1; 
+
+			}
 		}
 
-		if(karo>=5){
-			return 2; 
+		if (herz >= 5) {
+			return 0;
+		} else if (kreuz >= 5) {
+			return 1;
+		} else if (karo >= 5) {
+			return 2;
+		} else if (pik >= 5) {
+			return 3;
+		} else {
+			return 30000;
 		}
-		
-		if(pik>=5){
-			return 3; 
-		}
-		return 30000;}
-	
-	public int [] sameworth(int [] cardworth){
-		int same=1;
-		int same2=1;
+	}
+
+	/**
+	 * counts pairs, threelings and fourlings
+	 * 
+	 * @param cardworth
+	 * @return
+	 */
+	public int[] sameworth(int[] cardworth) {
+		int same = 1;
+		int same2 = 1;
 		int[] sameworth = new int[4];
+
+		// Durchläuft alle Karten
 		for (int a = 0; a < 7; a++) {
-			if(a!=0 && cardworth[a]==cardworth[a-1]){
-				same=same+1; }
-				if (same>=4){
-					sameworth[0]=same;
-					sameworth[1]=cardworth[a-1];
-					return sameworth; }
-				else {
-					if((same==3) && (a!=6) &&(cardworth[a]!=cardworth[a+1]) && (cardworth[a]==cardworth[a-2])){
-					sameworth[0]=same;
-					sameworth[1]=cardworth[a-1];}
-				else{
-					if((same==2) && (a!=6)&& (cardworth[a]!=cardworth[a+1])){
-					sameworth[0]=same;
-					sameworth[1]=cardworth[a];
+			if (a != 0 && cardworth[a] == cardworth[a - 1]) {
+				same = same + 1;
+			}
+			if (same >= 4) {
+				sameworth[0] = same;
+				sameworth[1] = cardworth[a - 1];
+				return sameworth;
+			} else {
+				if ((same == 3) && (a != 6)
+						&& (cardworth[a] != cardworth[a + 1])
+						&& (cardworth[a] == cardworth[a - 2])) {
+					sameworth[0] = same;
+					sameworth[1] = cardworth[a - 1];
+				} else {
+					if ((same == 2) && (a != 6)
+							&& (cardworth[a] != cardworth[a + 1])) {
+						sameworth[0] = same;
+						sameworth[1] = cardworth[a];
+					}
+
 				}
-				
-					}}}
-		
+			}
+		}
+
 		for (int a = 0; a < 7; a++) {
-			if(a!=0 && cardworth[a]==cardworth[a-1]&& sameworth[1]!=cardworth[a]){
-				same2=same2+1;
-				sameworth[2]=same2;
-				sameworth[3]=cardworth[a];
+			if (a != 0 && cardworth[a] == cardworth[a - 1]
+					&& sameworth[1] != cardworth[a]) {
+				same2 = same2 + 1;
+				sameworth[2] = same2;
+				sameworth[3] = cardworth[a];
 				return sameworth;
 			}
-			}
-		return sameworth;
 		}
-		
-	
+		return sameworth;
+	}
 
+	public int followfive(int[] cardworth) {
+		int follow = 1;
+		for (int a = 0; a < 7; a++) {
+			if (a != 0 && cardworth[a] == cardworth[a - 1] + 1) {
+				follow = follow + 1;
+			}
+			if (follow >= 5) {
+				return cardworth[a];
+			}
+		}
+		return 0;
 
-
+	}
 
 }
-
-	
